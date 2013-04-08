675bd66aac39e34ec13a94e9bb1ca8560ed279e3
Remco Tolsma <info@remcotolsma.nl>
2013-04-08 13:53:31 +0200

    Update to 0.1.3.

---
+++ deploy-git-to-svn.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Deploy WordPress plugin from GitHub to Subversion
+Plugin URI: http://remcotolsma.nl/
+Description: Deploy WordPress plugin from GitHub to Subversion.
+ 
+Version: 0.1.3
+Requires at least: 3.0
+
+Author: Remco Tolsma
+Author URI: http://remcotolsma.nl/
+
+Text Domain: deploy_wp__git_to_svn
+Domain Path: /languages/
+
+License: GPL
+
+GitHub URI: https://github.com/pronamic/wp-pronamic-ideal
+*/
+
---
+++ readme.txt
@@ -0,0 +1,6 @@
+=== Deploy WordPress plugin from GitHub to Subversion ===
+Contributors: remcotolsma 
+Tags: wordpress, plugin, deploy, github, subversion
+Requires at least: 3.0
+Tested up to: 3.5.1
+Stable tag: 0.1.3
